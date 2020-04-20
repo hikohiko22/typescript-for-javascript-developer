394fe06354099f192ae078dd08903ff5c665b431
Mitsu Narita <naritamitsutaka@naritamitsutakanoMacBook-Pro.local>
2020-04-20 13:33:08 +0900

    update

---
+++ src/number-string.ts
@@ -0,0 +1,14 @@
+export {};
+
+let year: number = 1991;
+//year = false;
+
+console.log(year);
+
+let age: number = 0x19;
+
+console.log(age);
+
+let name: string = 'mitsu';
+//name = 10;
+console.log(name);
