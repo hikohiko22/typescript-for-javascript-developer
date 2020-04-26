6b5bf798165357ab1176bffd3dad040bbe5ccec8
Mitsu Narita <naritamitsutaka@naritamitsutakanoMacBook-Pro.local>
2020-04-26 18:56:36 +0900

    aa

---
+++ src/object.ts
@@ -0,0 +1,9 @@
+export {};
+
+let profile1: object = { name: 'mitsu' };
+profile1 = { birthYear: 1991 };
+
+let profile2: {
+  name: string;
+} = { name: 'mitsu' };
+//profile2 = { birthYear: 1991 };
